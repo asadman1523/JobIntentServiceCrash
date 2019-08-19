113e58fdab9f51c4900dbe5b956dd2e6fe6ba5cf
Julio Cataño <julio.catano@globant.com>
2019-08-18 21:54:37 -0400

    test with crash on jobintentservice due to pending intent

--- app/src/main/java/com/example/jobintentservicetest/TestJobService.kt
+++ app/src/main/java/com/example/jobintentservicetest/TestJobService.kt
@@ -7,12 +7,12 @@ import androidx.core.app.JobIntentService
 
 class TestJobService : JobIntentService() {
     override fun onHandleWork(intent: Intent) {
-        val dataClass = intent.takeIf { intent.hasExtra(TEST_DATA) }?.getParcelableExtra<TestDataClass>(TEST_DATA)
+        val idExtra = intent.takeIf { intent.hasExtra(DATA_ID_EXTRA) }?.getIntExtra(DATA_ID_EXTRA, -1)
 
-        if (dataClass != null) {
-            Log.d(TestJobService::class.java.canonicalName, "Obtained Data Class")
+        if (idExtra != null && idExtra != -1) {
+            Log.d(TestJobService::class.java.canonicalName, "Obtained Id Extra")
         } else {
-            Log.d(TestJobService::class.java.canonicalName, "Didn't Obtained Data Class")
+            Log.d(TestJobService::class.java.canonicalName, "Didn't Id Extra")
         }
 
         try {
@@ -24,11 +24,13 @@ class TestJobService : JobIntentService() {
 
     companion object {
         private const val ID = 82313
-        private const val TEST_DATA = "TEST_DATA"
+        private const val DATA_ID_EXTRA = "ID_EXTRA"
+        private const val DATA_MSG_EXTRA= "MSG_EXTRA"
 
         fun startTestService(context: Context, data: TestDataClass) {
             val intent = Intent(context, TestJobService::class.java)
-            intent.putExtra(TEST_DATA, data)
+            intent.putExtra(DATA_ID_EXTRA, data.randomId)
+            intent.putExtra(DATA_MSG_EXTRA, data.testMessage)
 
             enqueueWork(context, TestJobService::class.java, ID, intent)
         }
